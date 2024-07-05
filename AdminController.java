1f6f81cba7be82c8fc9150c0374ce9849b11d9b3
sumane <sumangq5@gmail.com>
2024-07-05 17:08:17 -0400

    2nd

---
+++ AdminController.java
@@ -0,0 +1,16 @@
+package com.example.lab3;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AdminController {
+    public Button view;
+    public Button create;
+    public Button update;
+    public Button delete;
+    public TextField id;
+
+    public TextField country;
+    public TextField gender;
+    public TextField name;
+}
