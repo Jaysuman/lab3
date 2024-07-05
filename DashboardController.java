2343c91745f620e1978b6d867bb872aaedc50f21
sumane <sumangq5@gmail.com>
2024-07-05 17:09:04 -0400

    3rd

---
+++ DashboardController.java
@@ -0,0 +1,32 @@
+package com.example.lab3;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class DashboardController {
+    public Label welcomeText;
+    public Button admin;
+    public Button employee;
+    public Button logout;
+    public Button exit;
+
+
+    public void onAdminButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void exit(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+
+
+    public void employee(ActionEvent actionEvent) {
+    }
+
+    public void admin(ActionEvent actionEvent) {
+    }
+
+    public void logout(ActionEvent actionEvent) {
+    }
+}
